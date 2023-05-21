ec0517ba56b2fa73cdaf07c291780dd420916fee
YASH-PRAJAPATI-4903 <114235226+YASH-PRAJAPATI-4903@users.noreply.github.com>
2023-05-21 17:37:40 +0530

    Add project files.

---
+++ Final-Flagnav/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Final_Flagnav.Models;
+using System.Data.Entity;
+
+namespace Final_Flagnav.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (var db = new modeEntities())
+            {
+                var d = db.MainMenus.Include(c => c.SubMenus).ToList();
+                return View(d);
+            }
+        }
+    }
+}
---
+++ Final-Flagnav/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Final_Flagnav.MvcApplication" Language="C#" %>
